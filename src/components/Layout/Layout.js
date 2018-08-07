88ae823db8637418daf97bd43a3ac7158f91ae5a
Neha Chauhan <nehachauhan79@gmail.com>
2018-08-07 22:39:39 +0530

    First Commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const layout = (props) => {
+        return(
+            <Aux>
+                <div> Toolbox , Sidedrawer, Backdrop </div>
+                 <main>
+                    {props.children}
+                </main>
+            </Aux>
+        );
+}
+
+
+export default layout ;
+
